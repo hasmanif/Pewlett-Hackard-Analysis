9edb178e079228f28ad2906747fd2633db6f42ab
hasmanif <77994818+hasmanif@users.noreply.github.com>
2021-04-11 16:37:15 -0400

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -27,3 +27,22 @@ INTO retiring_titles
 FROM retirement_clean AS r
 GROUP BY r.title
 ORDER BY count DESC;
+
+-- Mentorship Eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+     e.first_name,
+     e.last_name,
+	 e.birth_date,
+	 d.from_date,
+	 d.to_date,
+	 ti.title
+INTO mentorship_eligible
+FROM employees AS e
+INNER JOIN dept_emp AS d
+ON e.emp_no = d.emp_no
+INNER JOIN titles AS ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (d.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
